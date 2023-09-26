353cf523a4a3614d16d3ccbfcfb7d938c58c80f2
Chachanidze29 <chachanidze29m@gmail.com>
2023-09-26 15:31:11 +0400

    create registration route, make serializer for user

--- users/views.py
+++ users/views.py
@@ -1,9 +1,22 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import User
+from rest_framework import exceptions
+from .serializers import UserSerializer
+
+
+@api_view(["POST"])
+def register(request):
+    data = request.data
+    if (data['password'] != data['password_confirm']):
+        raise exceptions.APIException("Passwords don't match")
+    serializer = UserSerializer(data=data)
+    serializer.is_valid(raise_exception=True)
+    serializer.save()
+    return Response(serializer.data)
 
 
 @api_view(["GET"])
 def users(request):
-    users = User.objects.all()
-    return Response(users)
+    serializer = UserSerializer(User.objects.all(), many=True)
+    return Response(serializer.data)
