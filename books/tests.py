5d25ec3ffb4b9ec5f501480f4a001a4fc269946c
Chachanidze29 <chachanidze29m@gmail.com>
2023-09-30 20:04:24 +0400

    create tests for user and book model

--- books/tests.py
+++ books/tests.py
@@ -1,3 +1,65 @@
 from django.test import TestCase
+from .models import Book
+from users.models import User
 
-# Create your tests here.
+
+class BookModelTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(
+            email="test@example.com",
+            password="testpassword"
+        )
+
+    def test_create_book(self):
+        book = Book.objects.create(
+            name="Test Book",
+            author="Test Author",
+            genre="Test Genre",
+            condition="Good",
+            image="test.jpg",
+            location="Test Location",
+            owner=self.user,
+        )
+
+        self.assertIsInstance(book, Book)
+
+        self.assertEqual(book.name, "Test Book")
+        self.assertEqual(book.author, "Test Author")
+        self.assertEqual(book.genre, "Test Genre")
+        self.assertEqual(book.condition, "Good")
+        self.assertEqual(book.image, "test.jpg")
+        self.assertEqual(book.location, "Test Location")
+        self.assertEqual(book.owner, self.user)
+
+    def test_recipient_can_be_null(self):
+        book = Book.objects.create(
+            name="Test Book",
+            author="Test Author",
+            genre="Test Genre",
+            condition="Good",
+            image="test.jpg",
+            location="Test Location",
+            owner=self.user,
+            recipient=None,
+        )
+
+        self.assertIsNone(book.recipient)
+
+    def test_recipient_can_be_set(self):
+        another_user = User.objects.create(
+            email="another@example.com",
+            password="anotherpassword"
+        )
+
+        book = Book.objects.create(
+            name="Test Book",
+            author="Test Author",
+            genre="Test Genre",
+            condition="Good",
+            image="test.jpg",
+            location="Test Location",
+            owner=self.user,
+            recipient=another_user,
+        )
+
+        self.assertEqual(book.recipient, another_user)
--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,35 @@
 from django.test import TestCase
+from .models import User
 
-# Create your tests here.
+
+class UserModelTestCase(TestCase):
+    def test_create_user(self):
+        user = User.objects.create(
+            first_name="John",
+            last_name="Doe",
+            email="john@example.com",
+            password="password123"
+        )
+
+        self.assertIsInstance(user, User)
+
+        self.assertEqual(user.first_name, "John")
+        self.assertEqual(user.last_name, "Doe")
+        self.assertEqual(user.email, "john@example.com")
+        self.assertEqual(user.password, "password123")
+
+    def test_unique_email(self):
+        User.objects.create(
+            first_name="Jane",
+            last_name="Doe",
+            email="jane@example.com",
+            password="password456"
+        )
+
+        with self.assertRaises(Exception):
+            User.objects.create(
+                first_name="Another",
+                last_name="User",
+                email="jane@example.com",
+                password="password789"
+            )
